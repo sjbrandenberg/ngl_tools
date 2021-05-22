6ea1162a8fdfa92243e34a195b0431429c601413
sjbrandenberg <sjbrandenberg@ucla.edu>
2021-05-22 15:32:44 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ngl'
+project = 'ngl_tools'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
