b12fada90bc6d34c525ffe38ecb22aa04af9e5c2
sjbrandenberg <sjbrandenberg@ucla.edu>
2021-05-22 15:18:34 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -31,6 +31,7 @@ release = '1.0.0'
 extensions = [
     "sphinx_rtd_theme",
     "recommonmark",
+    "sphinx_markdown_tables",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
